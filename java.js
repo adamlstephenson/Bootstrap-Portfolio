55c7c092f35a3737556b72404f945f6f39155d17
Adam Stephenson <adamstephenson@Adams-MacBook-Pro.local>
2018-03-11 21:12:35 -0500

    added portfolio functionality

--- java.js
+++ java.js
@@ -1,3 +1,8 @@
 document.getElementById("hangman").addEventListener('click', function() {
     location.href = "https://minnesnowman.github.io/Hangman-Game/"
 }, false);
+
+document.getElementById("crystalscollector").addEventListener('click', function() {
+    location.href = "https://minnesnowman.github.io/week-4-game/"
+}, false);
+
